6f46e12fae4ba51b8e546c75cc828ddad7e02e5f
Christopher Lux <christopherlux@gmail.com>
2007-04-10 11:06:28 +0000

    added missing shader files

---
+++ src/app_basic_volume_renderer/volume_renderer/shader/vol_raycast_cp_frag.glsl
@@ -0,0 +1,15 @@
+
+#version 110
+
+uniform sampler3D   _volume;
+uniform sampler1D   _color_alpha;
+
+vec4 do_shading(in vec4 inp)
+{
+    return (texture1D(_color_alpha, inp.r));
+}
+
+void main()
+{
+    gl_FragColor = do_shading(texture3D(_volume, gl_TexCoord[0].xyz));
+}
