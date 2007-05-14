a8bbf9fc0acf5f5b53d6ac31c6ed26c748e78a44
Christopher Lux <christopherlux@gmail.com>
2007-05-14 12:21:16 +0000

    timing work started inside scm_core

---
+++ src/lib_scm_core/scm_core/time/detail/get_time.h
@@ -0,0 +1,13 @@
+
+#ifndef GET_TIME_H_INCLUDED
+#define GET_TIME_H_INCLUDED
+
+namespace scm {
+namespace detail {
+
+    inline double get_time();
+
+} // namespace detail
+} // namespace scm
+
+#endif //GET_TIME_H_INCLUDED
---
+++ src/lib_scm_core/scm_core/time/timer.h
@@ -0,0 +1,9 @@
+
+#ifndef TIMER_H_INCLUDED
+#define TIMER_H_INCLUDED
+
+namespace scm {
+
+} // namespace scm
+
+#endif // TIMER_H_INCLUDED
