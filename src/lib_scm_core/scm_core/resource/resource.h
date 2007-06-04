0be53bf483a414529597ccd7bdffed3c0dca8e79
Christopher Lux <christopherlux@gmail.com>
2007-06-04 11:35:29 +0000

    !WIP! resource management work

--- src/lib_scm_core/scm_core/resource/resource.h
+++ src/lib_scm_core/scm_core/resource/resource.h
@@ -18,29 +18,34 @@ class resource_manager;
 
 class __scm_export resource_base
 {
+protected:
+    typedef boost::weak_ptr<resource_base>          resource_ptr;
+    typedef boost::weak_ptr<resource_manager_base>  manager_ptr;
+
 public:
     resource_base();
     resource_base(const resource_base& /*res*/);
     virtual ~resource_base();
 
-    virtual const resource_base&    operator=(const resource_base& /*rhs*/) = 0;
+    virtual const resource_base&    operator=(const resource_base& /*rhs*/);
 
                                     operator bool() const;
     bool                            operator !() const;
 
-    virtual                         swap(resource_base& /*ref*/)    = 0;
+    void                            swap(resource_base& /*ref*/);
 
 protected:
+    explicit resource_base(const resource_ptr&  /*res*/,
+                           const manager_ptr&   /*mgr*/);
 
 private:
-    boost::weak_ptr<resource_manager_base>  _manager;
+    resource_ptr                    _resource;
+    manager_ptr                     _manager;
 
     void                            register_instance();
     void                            release_instance();
 
 }; // class resource_base
-
-
 
 template<class res_type>
 class resource : public resource_base
@@ -50,23 +55,11 @@ public:
     resource(const resource& /*res*/);
     virtual ~resource();
 
-    const resource_base&            operator=(const resource_base& /*rhs*/);
-
-                                    operator bool() const;
-    bool                            operator !() const;
-
     res_type&                       get() const;
-    virtual void                    swap(resource_base& /*ref*/);
 
 private:
-    explicit resource(const boost::weak_ptr<res_type>& res,
-                      const boost::weak_ptr<resource_manager<res_type> >& mgr);
-
-    void                            register_instance();
-    void                            release_instance();
-
-    boost::weak_ptr<res_type>                       _resource;
-    boost::weak_ptr<resource_manager<res_type> >    _manager;
+    explicit resource(const resource_ptr&  /*res*/,
+                      const manager_ptr&   /*mgr*/);
 
     friend class resource_manager<res_type>;
 
--- src/lib_scm_core/scm_core/resource/resource.inl
+++ src/lib_scm_core/scm_core/resource/resource.inl
@@ -1,15 +1,15 @@
 
 #include <cassert>
 
-namespace std
-{
-    template<class res_type>
-    inline void swap(scm::res::resource<res_type>& lhs,
-                     scm::res::resource<res_type>& rhs)
-    {
-        lhs.swap(rhs);
-    }
-} // namespace std
+//namespace std
+//{
+//    template<class res_type>
+//    inline void swap(scm::res::resource<res_type>& lhs,
+//                     scm::res::resource<res_type>& rhs)
+//    {
+//        lhs.swap(rhs);
+//    }
+//} // namespace std
 
 namespace scm {
 namespace res {
@@ -19,6 +19,25 @@ resource<res_type>::resource()
 {
 }
 
+template<class res_type>
+resource<res_type>::resource(const resource<res_type>& res)
+  : resource_base(res)
+{
+}
+
+template<class res_type>
+resource<res_type>::resource(const resource_base::resource_ptr&  res,
+                             const resource_base::manager_ptr&   mgr)
+  : resource_base(res, mgr)
+{
+}
+
+template<class res_type>
+resource<res_type>::~resource()
+{
+}
+
+/*
 template<class res_type>
 resource<res_type>::resource(const resource<res_type>& res)
 {
@@ -38,6 +57,7 @@ resource<res_type>::~resource()
 {
     release_instance();
 }
+ 
 
 template<class res_type>
 const resource<res_type>&
@@ -63,6 +83,7 @@ inline bool resource<res_type>::operator !() const
 {
     return (!_resource.lock());
 }
+*/
 
 template<class res_type>
 inline res_type&
@@ -70,7 +91,7 @@ resource<res_type>::get() const
 {
     return (*_resource.lock());
 }
-
+/*
 template<class res_type>
 void resource<res_type>::register_instance()
 {
@@ -93,6 +114,6 @@ void resource<res_type>::swap(resource<res_type>& ref)
     std::swap(_manager,  ref._manager);
     std::swap(_resource, ref._resource);
 }
-
+*/
 } // namespace res
 } // namespace scm
