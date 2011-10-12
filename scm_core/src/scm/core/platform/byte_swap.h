4045e7cd070b0c4065a962bf748deb994109b666
Christopher Lux <christopherlux@gmail.com>
2011-10-12 13:54:50 +0000

    added byte_swap array variants

--- scm_core/src/scm/core/platform/byte_swap.h
+++ scm_core/src/scm/core/platform/byte_swap.h
@@ -19,67 +19,79 @@ namespace scm {
 
 inline
 void
-swap_bytes_1(void* d)
+swap_bytes_1(void* d, void* s)
 {
+    assert(s != 0);
     assert(d != 0);
+
+    uint8*const s8 = reinterpret_cast<uint8*>(s);
+    uint8*const d8 = reinterpret_cast<uint8*>(d);
+
+    *d8 = *s8;
 }
 
 inline
 void
-swap_bytes_2(void* d)
+swap_bytes_2(void* d, void* s)
 {
+    assert(s != 0);
     assert(d != 0);
 
+    uint16*const s16 = reinterpret_cast<uint16*>(s);
     uint16*const d16 = reinterpret_cast<uint16*>(d);
 
 #if    (SCM_PLATFORM == SCM_PLATFORM_WINDOWS) \
     && (_MSC_VER >= 1400)
-    *d16 = _byteswap_ushort(*d16);
+    *d16 = _byteswap_ushort(*s16);
 #else
-    *d16 =   (*d16 >> 8)
-           | (*d16 << 8);
+    *d16 =   (*s16 >> 8)
+           | (*s16 << 8);
 #endif // SCM_PLATFORM == SCM_PLATFORM_WINDOWS
 }
 
 inline
 void
-swap_bytes_4(void* d)
+swap_bytes_4(void* d, void* s)
 {
+    assert(s != 0);
     assert(d != 0);
 
+    uint32*const s32 = reinterpret_cast<uint32*>(s);
     uint32*const d32 = reinterpret_cast<uint32*>(d);
 
 #if    (SCM_PLATFORM == SCM_PLATFORM_WINDOWS) \
     && (_MSC_VER >= 1400)
-    *d32 = _byteswap_ulong(*d32);
+    *d32 = _byteswap_ulong(*s32);
 #else
-    *d32 =   ((*d32 & 0xff000000) >> 24)
-           | ((*d32 & 0x00ff0000) >> 8)
-           | ((*d32 & 0x0000ff00) << 8)
-           | ((*d32 & 0x000000ff) << 24);
+    *d32 =   ((*s32 & 0xff000000) >> 24)
+           | ((*s32 & 0x00ff0000) >> 8)
+           | ((*s32 & 0x0000ff00) << 8)
+           | ((*s32 & 0x000000ff) << 24);
 #endif // SCM_PLATFORM == SCM_PLATFORM_WINDOWS
 }
 
 inline
 void
-swap_bytes_8(void* d)
+swap_bytes_8(void* d, void* s)
 {
+    assert(s != 0);
     assert(d != 0);
 
+    uint64*const s64 = reinterpret_cast<uint64*>(s);
     uint64*const d64 = reinterpret_cast<uint64*>(d);
 
 #if    (SCM_PLATFORM == SCM_PLATFORM_WINDOWS) \
     && (_MSC_VER >= 1400)
-    *d64 = _byteswap_uint64(*d64);
+    *d64 = _byteswap_uint64(*s64);
 #else
-    *d64 =   ((*d64 & 0xff00000000000000ull) >> 56)
-           | ((*d64 & 0x00ff000000000000ull) >> 40)
-           | ((*d64 & 0x0000ff0000000000ull) >> 24)
-           | ((*d64 & 0x000000ff00000000ull) >> 8);
-           | ((*d64 & 0x00000000ff000000ull) << 8);
-           | ((*d64 & 0x0000000000ff0000ull) << 24);
-           | ((*d64 & 0x000000000000ff00ull) << 40);
-           | ((*d64 & 0x00000000000000ffull) << 56);
+    *d64 =   ((*s64 & 0xff00000000000000ull) >> 56)
+           | ((*s64 & 0x00ff000000000000ull) >> 40)
+           | ((*s64 & 0x0000ff0000000000ull) >> 24)
+           | ((*s64 & 0x000000ff00000000ull) >> 8);
+           | ((*s64 & 0x00000000ff000000ull) << 8);
+           | ((*s64 & 0x0000000000ff0000ull) << 24);
+           | ((*s64 & 0x000000000000ff00ull) << 40);
+           | ((*s64 & 0x00000000000000ffull) << 56);
 #endif // SCM_PLATFORM == SCM_PLATFORM_WINDOWS
 }
 
@@ -94,32 +106,32 @@ struct do_swap_bytes
 template<typename T>
 struct do_swap_bytes<T, 1>
 {
-    inline void operator()(T* d) {
-        swap_bytes_1(d);
+    inline void operator()(T* d, T* s) {
+        swap_bytes_1(d, s);
     }
 };
 
 template<typename T>
 struct do_swap_bytes<T, 2>
 {
-    inline void operator()(T* d) {
-        swap_bytes_2(d);
+    inline void operator()(T* d, T* s) {
+        swap_bytes_2(d, s);
     }
 };
 
 template<typename T>
 struct do_swap_bytes<T, 4>
 {
-    inline void operator()(T* d) {
-        swap_bytes_4(d);
+    inline void operator()(T* d, T* s) {
+        swap_bytes_4(d, s);
     }
 };
 
 template<typename T>
 struct do_swap_bytes<T, 8>
 {
-    inline void operator()(T* d) {
-        swap_bytes_8(d);
+    inline void operator()(T* d, T* s) {
+        swap_bytes_8(d, s);
     }
 };
 
@@ -129,7 +141,29 @@ swap_bytes(T* d)
 {
     BOOST_STATIC_ASSERT(sizeof(T) == 1 || sizeof(T) == 2 || sizeof(T) == 4 || sizeof(T) == 8);
     
-    do_swap_bytes<T, sizeof(T)>()(d);
+    do_swap_bytes<T, sizeof(T)>()(d, d);
+}
+
+template<typename T>
+inline void
+swap_bytes_array(T* d, scm::size_t c)
+{
+    BOOST_STATIC_ASSERT(sizeof(T) == 1 || sizeof(T) == 2 || sizeof(T) == 4 || sizeof(T) == 8);
+    
+    for (scm::size_t i = 0; i < c; ++i) {
+        do_swap_bytes<T, sizeof(T)>()(d + i, d + i);
+    }
+}
+
+template<typename T>
+inline void
+swap_bytes_array(T* d, T* s, scm::size_t c)
+{
+    BOOST_STATIC_ASSERT(sizeof(T) == 1 || sizeof(T) == 2 || sizeof(T) == 4 || sizeof(T) == 8);
+    
+    for (scm::size_t i = 0; i < c; ++i) {
+        do_swap_bytes<T, sizeof(T)>()(d + i, s + i);
+    }
 }
 
 } // namespace scm
